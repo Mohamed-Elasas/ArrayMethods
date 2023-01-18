5d54ff9d82777e684c6f6b7f923ca7a68476d017
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2023-01-18 22:32:28 +0200

    first end of the project

---
+++ app.js
@@ -0,0 +1,93 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+// getRandomUser();
+// getRandomUser();
+// getRandomUser();
+
+// fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+  // console.log(user);
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  addData(newUser);
+}
+
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // clear main div
+  main.innerHTML = `<h2><strong>Person</strong>Wealth</h2>`;
+
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<h3>${item.name}<strong>${formatMoney(
+      item.money
+    )}</strong></h3>`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money
+function formatMoney(number) {
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+}
+
+function sortByRichest() {
+  data.sort((a, b) => {
+    return b.money - a.money;
+  });
+  updateDOM();
+}
+
+function showMillionaries() {
+  data = data.filter((user) => user.money > 1000000);
+
+  updateDOM();
+}
+
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
+// Event Listeners
+addUserBtn.addEventListener("click", getRandomUser);
+
+doubleBtn.addEventListener("click", doubleMoney);
+
+sortBtn.addEventListener("click", sortByRichest);
+
+showMillionairesBtn.addEventListener("click", showMillionaries);
+
+calculateWealthBtn.addEventListener("click", calculateWealth);
